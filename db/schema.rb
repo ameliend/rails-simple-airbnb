7e0a74d76645f8088b306c31a0149c90a4fccda5
amelien delahaie <amelien.delahaie@gmail.com>
2020-12-10 00:21:52 +0100

    adding some css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_09_184559) do
+ActiveRecord::Schema.define(version: 2020_12_09_194758) do
 
   create_table "flats", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_12_09_184559) do
     t.integer "number_of_guests"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "picture_url"
   end
 
 end
